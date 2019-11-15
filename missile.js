ff48121b1dae7fd8408592a51483ee2fb7503eb1
dan-serraf <55733346+dan-serraf@users.noreply.github.com>
2019-11-15 13:23:56 +0100

    Update missile.js

--- missile.js
+++ missile.js
@@ -1,10 +1,11 @@
 class Missile {
 
-	constructor(x, y, speed, image, life) {
+	constructor(x, y, speed, image, image_super, life) {
 		this.x = x;
 		this.y = y;
 		this.speed = speed;
 		this.image = image;
+		this.image_super = image_super;
 		this.life = 0;
 		this.width = 5;
 		this.height = 17;
@@ -12,20 +13,32 @@ class Missile {
 
 	update(world, missile) {
 
-		this.y -= this.speed; // afin que le missile avance verticalement
+		missile.move();
+		missile.temps_de_vie(world, missile);
+		missile.supprime_missile(world, missile);
+		missile.collision_vaisseau_missile(world);
+	}
 
+	move() {
+		this.y -= this.speed; // afin que le missile avance verticalement
+	}
+
+	temps_de_vie() {
 		this.life += 10;
+
+	}
+
+	supprime_missile(world, missile) {
 		if (this.life > 300 || this.y === -100) {
-			/* dure de vie egale a la moitier du canvas*/
-			// enlever les missiles mort
 			world.missile.delete(missile);
 		}
+	}
 
-		// collision vaisseau avec missile
+	collision_vaisseau_missile(world) {
+
 		if (this.x < world.player.x + world.player.width && this.x + this.width > world.player.x && this.y < world.player.y + world.player.height && this.height + this.y > world.player.y) {
 			world.player.game = false;
 		}
-
 
 	}
 
@@ -35,3 +48,4 @@ class Missile {
 	}
 
 }
+
