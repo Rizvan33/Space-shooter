7b10bb0fc184e7ab09bafc8f70995584138d1a2e
dan-serraf <55733346+dan-serraf@users.noreply.github.com>
2019-11-24 21:08:02 +0100

    Update players.js

--- players.js
+++ players.js
@@ -6,54 +6,77 @@ class Player {
 		this.speed = speed;
 		this.game = game;
 		this.image = image;
-		this.keyboardManager = new KeyboardManager();
+		this.width = 40;
+		this.height = 40;
+
 
 	}
 
 	update(world) {
-		world.move = this.keyboardManager.update(world);
-		world.move.forEach(function (points) {
-				if (points == "up") {
+		world.player.sortie_canvas(world);
+		world.player.evenement(world);
 
-					this.y -= this.speed * world.delta;
-				} else if (points == "down") {
-					this.y += this.speed * world.delta;
-				} else if (points == "left") {
-					this.x -= this.speed * world.delta;
-				} else if (points == "right") {
-					this.x += this.speed * world.delta;
-				} else if (points == 'space') {
-						world.missile.add(new Missile(this.x, this.y - 30, this.speed / 40, view("laser.png"))); // -30 pour affiche le missile centre
-					
-				}
+	}
+
+	evenement(world) {
+
+
+		if (true == world.key.y1) {
+			this.y -= this.speed * world.delta;
+		}
+		if (true == world.key.y2) {
+			this.y += this.speed * world.delta;
+		}
+		if (true == world.key.x1) {
+			this.x -= this.speed * world.delta;
+		}
+		if (true == world.key.x2) {
+			this.x += this.speed * world.delta;
+		}
+		if (true == world.key.missile) {
+
+			if (world.niveau < 5) {
+
+				world.missile.add(new Missile(world.player.x + 18, world.player.y - 18, world.player.speed / 30, view("/images/laser.png"))); // -30 pour affiche le missile centre
+				//world.liste_missile.add([this.x, this.y - 40]); // -30 pour les prendres en compte dans l ordonne
+
+				// 2 missiles si au dessus de niveau 5
+			} else {
+
+				world.missile.add(new Missile(world.player.x - 10, world.player.y - 20, world.player.speed / 30, view("/images/laser.png"))); // -30 pour affiche le missile centre
+				world.missile.add(new Missile(world.player.x + 10, world.player.y - 20, world.player.speed / 30, view("/images/laser.png"))); // -30 pour affiche le missile centre
+
 			}
-			.bind(this));
 
-
-
+		}
+	}
+	
+	sortie_canvas(world) {
 		//si le vaisseau veux sortir du canvas on l'empeche de sortir
-		if (this.x > 600) {
-			this.x = 600;
+		world.player.sortie_horizontal();
+		world.player.sortie_vertical();
+
+	}
+
+	sortie_horizontal() {
+		if (this.x > 600 - this.width) {
+			this.x = 600 - this.width;
 		}
 		if (this.x < 0) {
 			this.x = 0;
 		}
-		if (this.y > 600) {
-			this.y = 600;
+	}
+
+	sortie_vertical() {
+		if (this.y > 600 - this.height) {
+			this.y = 600 - this.height;
 		}
 		if (this.y < 0) {
 			this.y = 0;
 		}
 	}
 
-
-
-
-
 	draw(contexte) {
-		contexte.drawImage(this.image, this.x - 20, this.y - 20, 40, 40); // -20 pour affiche le vaisseau centre 
-
-
+		contexte.drawImage(this.image, this.x, this.y, this.width, this.height); // -20 pour affiche le vaisseau centre 
 	}
-
 }
