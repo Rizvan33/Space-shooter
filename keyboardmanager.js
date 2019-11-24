dae15a39756d5ce1aeff15b85dcbd5cb7485968c
dan-serraf <55733346+dan-serraf@users.noreply.github.com>
2019-11-24 21:22:44 +0100

    Update keyboardmanager.js

--- keyboardmanager.js
+++ keyboardmanager.js
@@ -8,60 +8,61 @@ class KeyboardManager {
 			"RIGHT": 39,
 			"DOWN": 40,
 			"SPACE": 32,
+			//"SECRET": 71,
 			"ENTRE": 13
 
-		};
+		}; //initialise le keyboard
+		this.x1 = false;
+		this.x2 = false;
+		this.y1 = false;
+		this.y2 = false;
+		this.missile = false;
 
 	}
 
 	update(world) {
+		//console.log(world.key.keyCodes.ENTRE);
 		window.addEventListener('keydown', function (event) {
 			var e = event.keyCode;
+
 			if (e == this.keyCodes.RIGHT) {
-				world.move.add("right");
+				this.x2 = true;
 			}
 			if (e == this.keyCodes.UP) {
-				world.move.add("up");
+				this.y1 = true;
 			}
 			if (e == this.keyCodes.LEFT) {
-				world.move.add("left");
+				this.x1 = true;
 			}
 			if (e == this.keyCodes.DOWN) {
-				world.move.add("down");
+				this.y2 = true;
 			}
 			if (e == this.keyCodes.SPACE) {
-				world.move.add("space");
-			}
-			if (e == this.keyCodes.ENTRE) {
-				world.move.add("entre");
+				this.missile = true;
 			}
 
 		}.bind(this));
 
 		window.addEventListener('keyup', function (event) {
 			var e = event.keyCode;
+
 			if (e == this.keyCodes.RIGHT) {
-				world.move.delete("right");
+				this.x2 = false;
 			}
 			if (e == this.keyCodes.UP) {
-				world.move.delete("up");
+				this.y1 = false;
 			}
 			if (e == this.keyCodes.LEFT) {
-				world.move.delete("left");
+				this.x1 = false;
 			}
 			if (e == this.keyCodes.DOWN) {
-				world.move.delete("down");
+				this.y2 = false;
 			}
 			if (e == this.keyCodes.SPACE) {
-				world.move.delete("space");
-			}
-			if (e == this.keyCodes.ENTRE) {
-				world.move.delete("entre");
+				this.missile = false;
 			}
 
 		}.bind(this));
-
-		return world.move;
 
 	}
 
