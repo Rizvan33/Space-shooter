d747fcdb0f666b9f7ac0617ed742ad799d2fcc63
Rizvan33 <55735154+Rizvan33@users.noreply.github.com>
2019-11-25 02:01:12 +0100

    Update Pillule.js

--- Pillule.js
+++ Pillule.js
@@ -1,37 +1,60 @@
-class Pillule {
+class Pillule_vie {
 
-	constructor(x, y, image, image_super, life) {
+	constructor(x, y, speed, image, niveau) {
 		this.x = x;
 		this.y = y;
+		this.speed = speed;
 		this.image = image;
-		this.image_super = image_super;
-		this.life = 0;
-		this.width = 5;
-		this.height = 17;
+		this.width = 20;
+		this.height = 20;
+		this.niveau = niveau;
+		
 	}
 
-	update(world, pillule) {
+	update(world, pillule_vie) {
 
-		pillule.temps_de_vie(world, missile);
-		pillule.supprime_missile(world, missile);
-		pillule.collision_vaisseau_missile(world);
+		pillule_vie.move();
+		pillule_vie.supprime_enemies(world, pillule_vie);
 	}
 
-	temps_de_vie() {
-		this.life += 10;
-	}
-
-	supprime_pillule(world, pillule) {
-		if (this.life > 300 || this.y === -100) {
-			world.pillule.delete(pillule);
+	move() {
+		if (this.niveau>=1 && this.niveau<5) {
+            this.x += this.speed; 
+            this.y -= this.speed; 
+		} else if (this.niveau <= 7) {
+            let v = Math.floor((7) * Math.random());
+            if((v==0)||(v==1)){
+		this.x -= this.speed;
+                this.y -= this.speed;
+            }
+            if((v==2)||(v==3)){
+                this.x += this.speed; 
+                this.y -= this.speed;
+            }
+		} else {
+            let v2 = Math.floor((7) * Math.random());
+            if(v2==0){
+		this.x -= this.speed;
+                this.y -= this.speed;
+            }
+            else if (v2==1) {
+                this.x += this.speed; 
+                this.y -= this.speed;
+            }
 		}
 	}
 
-	collision_vaisseau_pillule(world) {
+	supprime_enemies(world, pillule_vie) {
+		if (this.y === -100) {
+			world.pillule_vie.delete(pillule_vie);
+		}
+	}
 
-		if (this.x < world.player.x + world.player.width && this.x + this.width > world.player.x && this.y < world.player.y + world.player.height && this.height + this.y > world.player.y) {
+	collision_vaisseau_pillule_vie(world, pillule_vie) {
+
+		if (this.x == world.player.x && this.y == world.player.y) {
 			world.player.life += 1;
-      world.pillule.delete(pillule);
+      			world.pillule.delete(pillule_vie);
 		}
 
 	}
