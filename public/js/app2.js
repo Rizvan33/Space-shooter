231db2efe307c2ae1bfa6954a23d8605ee04afbb
Rizvan33 <55735154+Rizvan33@users.noreply.github.com>
2020-01-03 15:33:06 +0100

    Pour vérifier que les formulaires sont bien remplie

---
+++ public/js/app2.js
@@ -0,0 +1,19 @@
+document.forms["inscription"].addEventListener("submit", function (e) {
+	var erreur;
+	var inputs = this;
+	
+	for (var i = 0; i < inputs.length; i++) {
+		console.log(inputs[i]);
+		if (!inputs[i].value) {
+			erreur = "Veuillez renseigner tous les champs";
+			break;
+		}
+	}
+	if (erreur) {
+		e.preventDefault();
+		document.getElementById("erreur").innerHTML = erreur;
+		return false;
+	} else {
+		alert('Formulaire envoyé !');
+	}
+});
